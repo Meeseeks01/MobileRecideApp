b880711e35ecc7e42759f4da9013fa3b994f2ae0
Masky-ib <173140869+Masky-ib@users.noreply.github.com>
2026-05-20 15:04:54 +0300

    Implement recipe search screen logic

--- app/src/main/java/com/example/mobilerecipeapp/MainActivity.java
+++ app/src/main/java/com/example/mobilerecipeapp/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.mobilerecipeapp;
 
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobilerecipeapp.model.Meal;
+import com.example.mobilerecipeapp.network.MealApiClient;
+import com.example.mobilerecipeapp.ui.MealAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MealAdapter mealAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        RecyclerView mealsRecyclerView = findViewById(R.id.mealsRecyclerView);
+        SearchView searchView = findViewById(R.id.searchView);
+
+        mealAdapter = new MealAdapter();
+
+        mealsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mealsRecyclerView.setAdapter(mealAdapter);
+
+        loadMeals("Chicken");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadMeals(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void loadMeals(String query) {
+
+        MealApiClient.searchMeals(this, query, new MealApiClient.MealsCallback() {
+
+            @Override
+            public void onSuccess(List<Meal> meals) {
+                mealAdapter.submitList(meals);
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Failed to load recipes",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
         });
     }
 }
