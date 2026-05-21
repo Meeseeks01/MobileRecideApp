fce0a8a7d21a1a12fc999d9469c594912fb4fd32
Masky-ib <173140869+Masky-ib@users.noreply.github.com>
2026-05-21 13:30:13 +0300

    Add Room database dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
     implementation("com.android.volley:volley:1.2.1")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
 }
